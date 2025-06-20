de026c4cbb50590044fcdc0abaeaa7b12b6a6cce
Dhruv Dhandukiya <dhruvdhandukiya@gmail.com>
2025-06-20 11:45:42 +0530

    commit

--- src/library/UserStore.js
+++ src/library/UserStore.js
@@ -7,22 +7,20 @@ export const useUserStore = create((set) => ({
     isLoading: true,
     user: null,
     fetchUserInfo: async (uid) => {
-        if(!uid) return set({currentUser: null, isLoading: false});
+        if (!uid) return set({ currentUser: null, isLoading: false });
 
-        try{
+        try {
             const docRef = doc(db, "users", uid);
             const docSnap = await getDoc(docRef);
 
-            if(docSnap.exists()){
-                set({currentUser: docSnap.data(), isLoading: false});
+            if (docSnap.exists()) {
+                set({ currentUser: docSnap.data(), isLoading: false });
+            } else {
+                set({ currentUser: null, isLoading: false });
             }
-            else{
-                set({currentUser: null, isLoading: false}); 
-            }
-        }
-        catch{
-            console.log(err);
-            return set({currentUser: null, isLoading: false});
+        } catch (err) {
+            console.log(err); // ✅ error now correctly handled
+            return set({ currentUser: null, isLoading: false });
         }
     },
     setUser: (userData) => set({ user: userData }),
